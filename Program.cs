5da262ddabc70130d56e0a7c2c70e09f9772d818
Leon Reitenbach <lreitenbach@schueler.hakzell.at>
2024-01-12 11:26:08 +0100

    git init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> zahlen = new List<int>();
+
+zahlen.Add(1);
+zahlen.Add(2);
+zahlen.Add(3);
+zahlen.Add(4);
+
+for (int i = 0; i < zahlen.Count; i++)
+{
+    Console.WriteLine($"{i + 1} + {i + 1} ergibt {(i + 1) + (i + 1)}");
+
+}
